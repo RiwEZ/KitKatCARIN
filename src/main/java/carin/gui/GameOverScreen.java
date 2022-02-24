9cfe55620749b42c10d0a7e9960cf8dd1c34ba9b
tananun <tananun_c@cmu.ac.th>
2022-02-24 15:18:48 +0700

    add: restart button, change variant name in class GameOverScreen

--- src/main/java/carin/gui/GameOverScreen.java
+++ src/main/java/carin/gui/GameOverScreen.java
@@ -15,7 +15,8 @@ public class GameOverScreen extends Screen {
     public static final Color COLOR_OUTLINE = new Color(0, 0, 0, 180);
     private static final Color COLOR_BG = new Color(70, 70, 70, 255);
     private static final Color COLOR_SHOP = new Color(30, 30, 30, 255);
-    private ImageComponent playButton;
+    private ImageComponent exitButton;
+    private ImageComponent restartButton;
     private boolean locked;
 
     public GameOverScreen(){super(NAME);}
@@ -32,33 +33,46 @@ public class GameOverScreen extends Screen {
     }
     protected void initializeComponents() {
         super.initializeComponents();
-        // exit button
+        // restart button
         double x = Game.window().getCenter().getX();
         double y = Game.window().getCenter().getY();
         double width = Game.window().getResolution().getWidth() / 3;
         double height = Game.window().getResolution().getWidth() / 6;
-        this.playButton = new ImageComponent(x - width / 2.0, y + height / 3.0, width, height);
-        this.playButton.setImage(null);
-        this.playButton.setText("EXIT");
-        this.playButton.setFont(Program.GUI_FONT.deriveFont(100f));
-        this.playButton.getAppearance().setForeColor(new Color(255,255,255));
-        this.playButton.getAppearanceHovered().setForeColor(new Color(215, 82, 82));
+        this.restartButton = new ImageComponent(x - width / 2.0, y - height / 3.0, width, height);
+        this.restartButton.setImage(null);
+        this.restartButton.setText("RESTART");
+        this.restartButton.setFont(Program.GUI_FONT.deriveFont(80f));
+        this.restartButton.getAppearance().setForeColor(new Color(255,255,255));
+        this.restartButton.getAppearanceHovered().setForeColor(new Color(215, 82, 82));
 
-        this.playButton.onClicked(e -> {
+        this.restartButton.onClicked(e -> {
             if (this.locked) {
                 return;
             }
             this.locked = true;
-            this.playButton.setEnabled(false);
+            this.restartButton.setEnabled(false);
             Game.window().getRenderComponent().fadeOut(1000);
             Game.loop().perform(1500, () -> {
                 displayMenuScreen();
                 Game.window().getRenderComponent().fadeIn(1000);
                 this.locked = false;
-                this.playButton.setEnabled(true);
+                this.restartButton.setEnabled(true);
             });
         });
-        this.getComponents().add(this.playButton);
+        this.getComponents().add(this.restartButton);
+
+        // exit button
+        this.exitButton = new ImageComponent(x - width / 2.0, y + height / 6.0, width, height);
+        this.exitButton.setImage(null);
+        this.exitButton.setText("EXIT");
+        this.exitButton.setFont(Program.GUI_FONT.deriveFont(75f));
+        this.exitButton.getAppearance().setForeColor(new Color(255,255,255));
+        this.exitButton.getAppearanceHovered().setForeColor(new Color(215, 82, 82));
+
+        this.exitButton.onClicked(e -> {
+            System.exit(0);
+        });
+        this.getComponents().add(this.exitButton);
     }
 
     private void renderGameOverLogo(Graphics2D g) {
